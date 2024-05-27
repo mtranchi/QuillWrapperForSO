cec63fe58084abe1edfd2b2ce860216bcd16e352
Mike Tranchida <Mtranchi@hotmail.com>
2024-05-27 10:32:39 -0400

    Add project files.

---
+++ QuillWrapperForSO/QuillWrapperForSO/wwwroot/quillWrapper.js
@@ -0,0 +1,31 @@
+﻿var quill;
+
+function initQuill(el) {
+    quill = new Quill(el, {
+        theme: 'snow'
+    });
+
+    el.onblur = function () {
+        editorBlurred();
+    }
+}
+
+// InputRichTextEditorDotnet notices the div losing focus and calls this method
+function getHtml() {
+    return quill.getSemanticHTML();
+}
+
+
+//Quill will bubble up the blur event to dotnet
+var GLOBAL = {};
+GLOBAL.DotNetReference = null;
+
+function setDotnetReference(pDotNetReference) {
+    GLOBAL.DotNetReference = pDotNetReference;
+}
+
+function editorBlurred() {
+    const html = quill.getSemanticHTML();
+    console.log('HTML FROM JS editorBlurred(): ' + html);
+    GLOBAL.DotNetReference.invokeMethodAsync('QuillCalled', html);
+}
